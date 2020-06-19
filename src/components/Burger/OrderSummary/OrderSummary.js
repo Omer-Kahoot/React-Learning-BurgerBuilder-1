702d8d5d3f793aac66594a3111487e2841f8fb89
Omer Kahoot <omer@mauqa.online>
2020-06-20 04:31:23 +0500

    Added backdrop and continue cancel buttons inside order summary modal dialog. also added a button component for customizing buttons.

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './OrderSummary.module.css'
+import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientsSummary = Object.keys(props.ingredients).map((mpKey)=>{
+        return <li key={mpKey}><span style={{textTransform: 'capitalize'}}>{mpKey}</span> : {props.ingredients[mpKey]}</li>
+    });
+
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientsSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
+        </Aux>
+    )
+};
+
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 const INGREDIENT_PRICES = {
@@ -24,7 +26,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState = (ingredients) => {
@@ -68,6 +71,19 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({purchasing : true});
+    }
+
+    purchaseCancelHandler = () => {
+        console.log('clicked');
+        this.setState({purchasing : false});
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!!');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -79,9 +95,19 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal 
+                    show={this.state.purchasing} 
+                    modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary 
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinue={this.purchaseContinueHandler}
+                        ingredients={this.state.ingredients}
+                        />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
                     ingredientAdded={this.addIngredientHandler}
