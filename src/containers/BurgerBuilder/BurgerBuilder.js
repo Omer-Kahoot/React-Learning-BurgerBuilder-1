8fab9b20e15a9b4f6cf24fd84edae8e4c676cf92
Omer Kahoot <omer@mauqa.online>
2020-06-20 03:32:06 +0500

    showing price at frontend for the burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -67,6 +67,7 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
+                    price={this.state.totalPrice}
                     disabled={disabledInfo}
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}/>
