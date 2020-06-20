ea3b1cf92dac0e76b5dd9f492694e30de754b1fc
Omer Kahoot <omer@mauqa.online>
2020-06-20 06:07:52 +0500

    Improving performance, changed z index for modal if it is not showing alongwith changes done for modal that no need to update if show isn't changed hence update will only run if show has been changed.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './OrderSummary.module.css'
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients).map((mpKey)=>{
-        return <li key={mpKey}><span style={{textTransform: 'capitalize'}}>{mpKey}</span> : {props.ingredients[mpKey]}</li>
-    });
+class OrderSummary extends Component {
+    //This could be a functional component, doesn't have to be a class component just for checking, this is done.
+    componentDidUpdate() {
+        console.log('[OrderSummary] Didupdate');
+    }
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    )
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients).map((mpKey) => {
+            return <li key={mpKey}><span style={{ textTransform: 'capitalize' }}>{mpKey}</span> : {this.props.ingredients[mpKey]}</li>
+        });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
