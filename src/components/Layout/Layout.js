3dedb21c7a339d7dea3b73522dc7c5467ccf209a
Omer Kahoot <omer@mauqa.online>
2020-06-20 05:53:02 +0500

    changes for completion of side drawer, drawer toggle button, navigation items, toolbar changes and logo inside the drwaer as well.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer : false
+    };
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer : false
+        });
+    }
+
+    sideBarOpenedHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer : !prevState.showSideDrawer };
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideBarOpenedHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+
+}
+
+export default Layout;
