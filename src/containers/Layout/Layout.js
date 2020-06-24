a3239856887d756490a760c53b9fcd4cdd7c0611
Omer Kahoot <omer@mauqa.online>
2020-06-25 01:32:44 +0500

    adding logout functionality initial for navigation items based on auth state

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
+import { connect } from 'react-redux';
 
 class Layout extends Component {
     state = {
@@ -24,8 +25,13 @@ class Layout extends Component {
     render() {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideBarOpenedHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <Toolbar 
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideBarOpenedHandler}/>
+                <SideDrawer 
+                    isAuth={this.props.isAuthenticated}
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
@@ -35,4 +41,10 @@ class Layout extends Component {
 
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated : state.auth.token !== null
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
